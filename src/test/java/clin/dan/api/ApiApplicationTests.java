6402427b8920277a107c1592b106beeadb26b0f6
Danilo Bruno <danilobsilv@gmail.com>
2024-05-18 23:38:00 -0400

    initial commit

---
+++ src/test/java/clin/dan/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package clin.dan.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
