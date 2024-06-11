bf9040d2d686abdcbe4284f8ff0677d482390eb3
Danilo Bruno <danilobsilv@gmail.com>
2024-06-11 17:30:35 -0400

    cors feat

--- src/main/java/clin/dan/api/CorsConfiguration/CorsConfiguration.java
+++ src/main/java/clin/dan/api/CorsConfiguration/CorsConfiguration.java
@@ -1,2 +1,17 @@
-package clin.dan.api.CorsConfiguration;public class CorsConfiguration {
+package clin.dan.api.CorsConfiguration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
 }
+
