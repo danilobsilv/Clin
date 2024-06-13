3142f1b597e72e39e76d521b729bce08c443c2ad
Danilo Bruno <danilobsilv@gmail.com>
2024-06-12 21:23:40 -0400

    add: getters

--- src/main/java/clin/dan/api/Features/MedicoFeatures/MedicoModel.java
+++ src/main/java/clin/dan/api/Features/MedicoFeatures/MedicoModel.java
@@ -25,6 +25,38 @@ public class MedicoModel {
     @Enumerated(EnumType.STRING)
     private Especialidade especialidade;
 
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getCrm() {
+        return crm;
+    }
+
+    public Especialidade getEspecialidade() {
+        return especialidade;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
     @Embedded
     private Endereco endereco;
 
